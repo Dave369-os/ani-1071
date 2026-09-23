b0994172db44a941dac1705be89d45c192f46ba5
unknown <ulrichallan45@gmail.com>
2026-09-23 23:34:37 +0100

    suite_de_collatz

---
+++ chapitre-02/exo10-la_suite_de_collatz/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+int main(){
+    int n;
+    int i = 0;
+    printf("entrez un entier superieur a 1 \n");
+    scanf("%d", &n);
+    while(n != 1){
+        if ( n % 2 == 0){
+            n = n/2;
+        }
+        else
+            n = 3*n + 1;   
+        i = i + 1;
+        printf("%d\t", n);
+    }
+    printf("\n%d etapes ont ete necessires pour arriver a 1.", i);
+
+    return 0;
+}
